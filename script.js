f2bbaf90e4eee369765160c843db7b5945b12f66
Mohhamed Ali <135774931+mohhamedali@users.noreply.github.com>
2024-02-08 14:57:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function draw() {
+  const canvas = document.getElementById("canvas");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    // Filled triangle
+    ctx.beginPath();
+    ctx.moveTo(25, 25);
+    ctx.lineTo(105, 25);
+    ctx.lineTo(25, 105);
+    ctx.fill();
+
+    // Stroked triangle
+    ctx.beginPath();
+    ctx.moveTo(125, 125);
+    ctx.lineTo(125, 45);
+    ctx.lineTo(45, 125);
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+draw();
 
